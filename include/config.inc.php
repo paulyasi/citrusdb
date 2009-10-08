7c847f3d64f2aa51d9fbe2e56fcf9add685de234
Paul Yasi <paul@citrusdb.org>
2009-10-08 10:14:01 -0400

    adding decrypt cards script

--- include/config.inc.php
+++ include/config.inc.php
@@ -13,5 +13,6 @@ $sys_dbtype = 'mysql';
 $path_to_citrus = '/home/pyasi/citrus_project/citrusdb-gpg';
 $hidden_hash_var='youmustchangethis';
 $lang = './include/local/us-english.inc.php';
-$gpg_command = "/usr/bin/gpg --armor --always-trust --batch --no-secmem-warning -e -u 'Paul Yasi' -r 'Paul Yasi'";
+$gpg_command = "/usr/bin/gpg --armor --always-trust --batch --no-secmem-warning -e -r 'Paul Yasi'";
+$gpg_decrypt = "/usr/bin/gpg --yes --decrypt";
 $path_to_home = '/home/pyasi'; // user with the gpg keyring
