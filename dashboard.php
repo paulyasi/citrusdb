e5f9036b2b0c330d4ffbf790ef7d230b408a7d61
Paul Yasi <paul@citrusdb.org>
2009-11-21 16:38:06 -0500

    change new messages item to work like a tab on dashboard

--- dashboard.php
+++ dashboard.php
@@ -15,64 +15,7 @@ if (!defined("INDEX_CITRUS")) {
         exit;
 }
 
-// Print Number of New Support Messages
 
-// query the customer_history for the number of 
-// waiting messages sent to that user
-$supportquery = "SELECT * FROM customer_history WHERE notify = '$user' ".
-  "AND status = \"not done\" AND date(creation_date) <= CURRENT_DATE";
-$supportresult = $DB->Execute($supportquery) or die ("$l_queryfailed");
-$num_rows = $supportresult->RowCount();
-
-$hasmessages = 0;
-
-echo "<hr><center>";
-
-if ($num_rows > 0) {
-  $hasmessages = 1;
-  if ($num_rows > 1) {
-    // plural messages
-    print "<b><a href=\"index.php?load=tickets&type=base\">$num_rows new messages for $user</b></a><br>";
-  } else {
-    // singular message
-    print "<b><a href=\"index.php?load=tickets&type=base\">$num_rows new message for $user</a></b><br>";
-  }
- }
-
-// query the customer_history for messages sent to 
-// groups the user belongs to
-$query = "SELECT * FROM groups WHERE groupmember = '$user' ";
-$supportresult = $DB->Execute($query) 
-  or die ("$l_queryfailed");
-
-while ($mygroupresult = $supportresult->FetchRow()) {
-  if (!isset($mygroupresult['groupname']))  { 
-    $mygroupresult['groupname'] = ""; 
-  }
-  
-  $groupname = $mygroupresult['groupname'];
-  $query = "SELECT * FROM customer_history WHERE notify = '$groupname' ".
-    "AND status = \"not done\" AND date(creation_date) <= CURRENT_DATE";
-  $gpresult = $DB->Execute($query) or die ("$l_queryfailed");
-  $num_rows = $gpresult->RowCount();
-  
-  if ($num_rows > 0) {
-    $hasmessges = 1;
-    if ($num_rows > 1) {
-      // plural messages
-      print "<b><a href=\"index.php?load=tickets&type=base\">$num_rows new messages for $groupname</a></b><br>";
-    } else {
-      // singular message
-      print "<b><a href=\"index.php?load=tickets&type=base\">$num_rows new message for $groupname</a></b><br>";      
-    }
-  }
-  
- }
-
-if ($hasmessages == 0) {
-  echo "<a href=\"index.php?load=tickets&type=base\">No New Messages</a><br>";
- }
-echo "</center><hr>";
 echo "<p align=center><b>Recently Viewed:</b>";
 echo "<table cellpadding=10><td class=\"smalltext\">";
 // show recent customers viewed
@@ -87,6 +30,71 @@ while ($myresult = $result->FetchRow()) {
 
  }
 echo "</td></table></p>";
-echo "<hr>";
-echo "show who is logged in down here";
+
+
+// Print Number of New Support Messages
+
+// query the customer_history for the number of 
+// waiting messages sent to that user
+$supportquery = "SELECT * FROM customer_history WHERE notify = '$user' ".
+  "AND status = \"not done\" AND date(creation_date) <= CURRENT_DATE";
+$supportresult = $DB->Execute($supportquery) or die ("$l_queryfailed");
+$num_rows = $supportresult->RowCount();
+
+$nummessages = 0;
+
+echo "<center>";
+
+$nummessages = $nummessaes + $num_rows;
+
+// query the customer_history for messages sent to 
+// groups the user belongs to
+$query = "SELECT * FROM groups WHERE groupmember = '$user' ";
+$supportresult = $DB->Execute($query) 
+  or die ("$l_queryfailed");
+
+while ($mygroupresult = $supportresult->FetchRow()) {
+  if (!isset($mygroupresult['groupname']))  { 
+    $mygroupresult['groupname'] = ""; 
+  }
+
+  // query each group
+  $groupname = $mygroupresult['groupname'];
+  $query = "SELECT * FROM customer_history WHERE notify = '$groupname' ".
+    "AND status = \"not done\" AND date(creation_date) <= CURRENT_DATE";
+  $gpresult = $DB->Execute($query) or die ("$l_queryfailed");
+  $num_rows = $gpresult->RowCount();
+
+  $nummessages = $nummessages + $num_rows;
+  
+ }
+
+// put the num messages link inside of tabnav
+
+echo "<div id=\"tabnav\">";
+
+if ($nummessages > 0) {
+  if ($nummessages > 1) {
+    // plural messages
+    if ($load == "tickets") {   
+      print "<div <a class = \"active\" href=\"index.php?load=tickets&type=base\">$nummessages new messages</a></div>";
+    } else {
+      print "<div <a href=\"index.php?load=tickets&type=base\">$nummessages new messages</a></div>";      
+    }
+  } else {
+    // singular message
+    if ($load == "tickets") {
+      print "<div><a class = \"active\" href=\"index.php?load=tickets&type=base\">$nummessages new message</a></div>";
+    } else {
+      print "<div><a href=\"index.php?load=tickets&type=base\">$nummessages new message</a></div>";      
+    }
+  }
+ } else {
+  if ($load == "tickets") {
+    echo "<div><a href=\"index.php?load=tickets&type=base\">No New Messages</a><div>";
+  } else {
+    echo "<div><a href=\"index.php?load=tickets&type=base\">No New Messages</a><div>";
+  }
+ }
+
 ?>
