67618bd2358be43f403149ebc456289146ba4761
Paul Yasi <paul@citrusdb.org>
2009-12-07 09:47:37 -0500

    added service growth queries and showing raw output for now

--- tools/servicegrowth.php
+++ tools/servicegrowth.php
@@ -16,53 +16,68 @@ if (!defined("INDEX_CITRUS")) {
 }
 
 
-// print out a graph that compares each of the last 12 months
-// of service start_dates compared to end_dates
-$current  = date("Y-m-d", mktime(0, 0, 0, date("m")  , date("d"), date("Y")));
-
-// TODO: do this for each master_service id
-$query = "SELECT monthname(us.start_datetime), year(start_datetime), count(*) FROM user_services us 
-  LEFT JOIN master_services ms ON ms.id = us.master_service_id 
-WHERE ms.id = 3 
-  GROUP BY YEAR(us.start_datetime),MONTH(us.start_datetime)";
-
-
-
-
 // GET Variables
-if (!isset($base->input['day'])) { $base->input['day'] = ""; }
-$day = $base->input['day'];
+if (!isset($base->input['year'])) { $base->input['year'] = ""; }
+$year = $base->input['year'];
 
-if ($day) {
-  // get the path_to_ccfile
-  $query = "SELECT path_to_ccfile FROM settings WHERE id = 1";
+if (!isset($base->input['category'])) { $base->input['category'] = ""; }
+$category = $base->input['category'];
+
+if ($year) {
+  // print out a graph that compares each of the last 12 months
+  // of service start_dates compared to end_dates
+  $current  = date("Y-m-d", mktime(0, 0, 0, date("m")  , date("d"), date("Y")));
+  echo "$category";
+  echo "<table><td>";
+  echo "started<br>";  
+  $query = "SELECT monthname(us.start_datetime) AS month, year(start_datetime), ".
+    "count(*) AS count FROM user_services us ".
+    "LEFT JOIN master_services ms ON ms.id = us.master_service_id ".
+    "WHERE ms.category = '$category' AND YEAR(us.start_datetime) = '$year'".
+    "GROUP BY MONTH(us.start_datetime)";
   $DB->SetFetchMode(ADODB_FETCH_ASSOC);
   $result = $DB->Execute($query) or die ("$l_queryfailed");
-  $myresult = $result->fields;
-  $mydir = $myresult['path_to_ccfile'];
-
-  // the pdfs are all named with
-  // -2009-08-10.pdf at the end
-  $pdfname = "-$day.pdf";
-  
-  // check if it is a pdf file that we allow anyone to open
-  if ($handle = opendir($mydir)) {
-    while (false !== ($file = readdir($handle))) {
-      if (substr($file,-15) == $pdfname) {
-	echo "<a href=\"http://db.gis.net/index.php?load=tools/downloadfile&type=dl&filename=$file\">$file</a><br>\n";
-      }
-    }
-    closedir($handle);
+  while ($myresult = $result->FetchRow()) {
+    $month = $myresult['month'];
+    $count = $myresult['count'];
+    echo "$month: $count<br>\n";
   }
-  
-} else {
+  echo "</td><td>";
+  echo "ended<br>";
+  $query = "SELECT monthname(us.end_datetime) AS month, year(end_datetime), ".
+    "count(*) AS count FROM user_services us ".
+    "LEFT JOIN master_services ms ON ms.id = us.master_service_id ".
+    "WHERE ms.category = '$category' AND YEAR(us.end_datetime) = '$year'".
+    "GROUP BY MONTH(us.end_datetime)";
+  $DB->SetFetchMode(ADODB_FETCH_ASSOC);
+  $result = $DB->Execute($query) or die ("$l_queryfailed");
+  while ($myresult = $result->FetchRow()) {
+    $month = $myresult['month'];
+    $count = $myresult['count'];
+    echo "$month: $count<br>\n";
+  }  
 
+  echo "</table>";
+
+ } else {
+  
   // print a form to ask what date you want to view
-  echo "Enter date of pdf notices to view:";
+  echo "Enter year of to see service growth:";
 echo "<FORM ACTION=\"index.php\" METHOD=\"GET\">".
-  "Date: <input type=text name=\"day\" value=\"$day\">".
-  "<input type=hidden name=type value=tools>".
-  "<input type=hidden name=load value=listpdf>".
+  "Year: <input type=text name=\"year\" value=\"$year\">".
+  "Category: <select name=\"category\">";
+// print list of categories to choose from
+ $query = "SELECT DISTINCT category FROM master_services ".
+   "ORDER BY category";
+ $DB->SetFetchMode(ADODB_FETCH_ASSOC);
+ $result = $DB->Execute($query) or die ("$l_queryfailed");
+ while ($myresult = $result->FetchRow()) {
+   $categoryname = $myresult['category'];
+   echo "<option value=\"$categoryname\">$categoryname</option> \n";
+ }
+ 
+echo  "</select><input type=hidden name=type value=tools>".
+  "<input type=hidden name=load value=servicegrowth>".
   "&nbsp;<input type=submit name=\"$l_submit\" value=\"submit\">".
   "</form> <p>";
 }
