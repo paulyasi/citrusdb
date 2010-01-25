abc75fd74516cc298fd1b405927a0dce1bee7e38
Paul Yasi <paul@citrusdb.org>
2010-01-25 15:02:07 -0500

    add billing_id and original invoice from dates and due date to detail report

--- billing_details.php
+++ billing_details.php
@@ -27,11 +27,17 @@ echo "<table cellspacing=0 cellpadding=4 border=0>".
 "<td bgcolor=\"#dddddd\" width=130><b>$l_date</b></td>".
 "<td bgcolor=\"#dddddd\" width=200><b>$l_description</b></td>".
 "<td bgcolor=\"#dddddd\" width=100><b>$l_invoice($l_original)</b></td>".
+"<td bgcolor=\"#dddddd\" width=100><b>$l_billingid</b></td>".
+"<td bgcolor=\"#dddddd\" width=100><b>$l_from</b></td>".
+"<td bgcolor=\"#dddddd\" width=100><b>$l_to</b></td>".
+"<td bgcolor=\"#dddddd\" width=100><b>$l_duedate</b></td>".
 "<td bgcolor=\"#dddddd\" width=100><b>$l_billedamount</b></td>".
 "<td bgcolor=\"#dddddd\" width=150><b>$l_paidamount</b></td>";
 
 // get the billing_details for this account
 // the account number is stored in the corresponding billing record
+
+//$DB->debug = true;
 
 $query = "SELECT d.id d_id, d.billing_id d_billing_id, ".
   "d.creation_date d_creation_date, d.user_services_id d_user_services_id, ".
@@ -39,16 +45,19 @@ $query = "SELECT d.id d_id, d.billing_id d_billing_id, ".
   "d.billed_amount d_billed_amount, d.paid_amount d_paid_amount, ".
   "d.refund_amount d_refund_amount, d.refunded d_refunded, ".
   "d.rerun d_rerun, d.original_invoice_number d_original_invoice, ".
-  "m.service_description m_description, r.description r_description ".
+  "m.service_description m_description, r.description r_description, ".
+  "bh.from_date bh_from_date, bh.to_date bh_to_date, ".
+  "bh.payment_due_date bh_due_date ".
   "FROM billing_details d ".
   "LEFT JOIN billing b ON b.id = d.billing_id ".
+  "LEFT JOIN billing_history bh ON bh.id = d.original_invoice_number ".
   "LEFT JOIN user_services u ON u.id = d.user_services_id ".
   "LEFT JOIN master_services m ON m.id = u.master_service_id ".
   "LEFT JOIN taxed_services t ON t.id = d.taxed_services_id ".
   "LEFT JOIN tax_rates r ON t.tax_rate_id = r.id ".
   "WHERE b.account_number = '$account_number' ORDER BY d.id DESC LIMIT 400";
 $DB->SetFetchMode(ADODB_FETCH_ASSOC);
-$result = $DB->Execute($query) or die ("$l_queryfailed");
+$result = $DB->Execute($query) or die ("$query $l_queryfailed");
 
 while ($myresult = $result->FetchRow()) {
   $id = $myresult['d_id'];
@@ -68,6 +77,10 @@ while ($myresult = $result->FetchRow()) {
   $refundamount = sprintf("%.2f",$myresult['d_refund_amount']);
   $rerun = $myresult['d_rerun'];
   $original_invoice = $myresult['d_original_invoice'];
+  $billing_id = $myresult['d_billing_id'];
+  $from_date = $myresult['bh_from_date'];
+  $to_date = $myresult['bh_to_date'];
+  $due_date = $myresult['bh_due_date'];
 
   print "<tr bgcolor=\"#eeeeee\">";
   print "<td style=\"border-top: 1px solid grey;\">$id &nbsp;</td>";
@@ -76,6 +89,10 @@ while ($myresult = $result->FetchRow()) {
   print "<td style=\"border-top: 1px solid grey;\">[ <a href=\"index.php?load=tools/modules/billing/htmlpreviousinvoice&billingid=$account_number&invoiceid=$invoice&details=on&type=fs&submit=on\" target=\"_blank\">$invoice</a> ]($original_invoice)</td>";	
 
   //print "<td style=\"border-top: 1px solid grey;\">$invoice &nbsp;</td>";
+  print "<td style=\"border-top: 1px solid grey;\">$billing_id</td>";
+  print "<td style=\"border-top: 1px solid grey;\">$from_date</td>";
+  print "<td style=\"border-top: 1px solid grey;\">$to_date</td>";
+  print "<td style=\"border-top: 1px solid grey;\">$due_date</td>";
 
   print "<td style=\"border-top: 1px solid grey;\">$billedamount &nbsp;</td>";
   print "<td style=\"border-top: 1px solid grey;\">$paidamount &nbsp;";
