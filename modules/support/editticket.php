730086a21e4ca15dc7c37642da863640f39b53d7
Paul Yasi <paul@citrusdb.org>
2010-03-16 09:55:29 -0400

    dissallow editing own ticket description once ticket is made pending or completed

--- modules/support/editticket.php
+++ modules/support/editticket.php
@@ -217,8 +217,8 @@ if ($savechanges) {
 	<a href=\"$linkurl\">$linkname</a></td><tr>
 
 	<td bgcolor=\"#ccccdd\"><b>$l_description</b></td><td colspan=3 bgcolor=\"#ddddee\">";
-  if ($user == $created_by) {
-    // let the user edit their own descriptions
+  if (($user == $created_by) && ($status != 'completed') && ($status != 'pending')) {
+    // let the user edit their own descriptions if not yet completed or pending
     echo "<textarea name=\"description\" rows=2 cols=70>$description</textarea></td><tr>";
   } else {
     echo "<br>$description<input type=hidden name=\"description\" value=\"$description\"<br><br></td><tr>";
