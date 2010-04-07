ed49801d9d416f67aba3001e3b0d435557695644
Paul Yasi <paul@citrusdb.org>
2010-04-07 11:33:38 -0400

    fix sql query

--- citrus.sql
+++ citrus.sql
@@ -857,8 +857,7 @@ CREATE TABLE `master_field_assets` (
     `id` INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY , 
     `description` VARCHAR( 128 ) NOT NULL , 
     `status` ENUM( 'current', 'old' ) NOT NULL DEFAULT 'current', 
-    `weight` FLOAT NULL , 
-    PRIMARY KEY ( `id` ) 
+    `weight` FLOAT NULL 
     ) ENGINE = MYISAM;  
 
 CREATE TABLE `field_asset_items` (
--- update.php
+++ update.php
@@ -831,8 +831,7 @@ if ($base->input['submit'] == "Update")
 	    "`id` INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY , ".
 	    "`description` VARCHAR( 128 ) NOT NULL , ".
 	    "`status` ENUM( 'current', 'old' ) NOT NULL DEFAULT 'current', ".
-	    "`weight` FLOAT NULL , ".
-	    "PRIMARY KEY ( `id` ) ".
+	    "`weight` FLOAT NULL ".
 	    ") ENGINE = MYISAM  ";
 	  $result = $DB->Execute($query) or die ("$query failed");
 	  echo "$query<br>\n";
