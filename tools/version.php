3d1e46519bb340a467f11b9c530aee2528e97ee0
Paul Yasi <paul@citrusdb.org>
2010-05-17 12:11:47 -0400

    including the README text in the version tool

--- tools/version.php
+++ tools/version.php
@@ -40,19 +40,7 @@ $serverinfo = $DB->ServerInfo();
 <hr width=450 noshade size=1>
 <br><br><br>
 <table><td width=400>
-<font size="-2">
-CitrusDB - The Open Source Customer Database<br>
-Copyright (C) 2002-2009 Paul Yasi<br>
-<p>
-This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
-<p>
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-<p>
-You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-<p>
-http://www.citrusdb.org<br>
-Read the README file for more details
-</font>
+<pre><? include ('./README'); ?></pre>
 </td>
 </table>
 </center>
