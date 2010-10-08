2fc09d63fc3221000991fa281bbd04ce205b0197
Paul Yasi <paul@citrusdb.org>
2010-10-08 12:33:09 -0400

    adding link to edit the removal date in the service history

--- modules/services/history.php
+++ modules/services/history.php
@@ -6,16 +6,52 @@
 // Check for authorized accesss
 /*----------------------------------------------------------------------------*/
 if(constant("INDEX_CITRUS") <> 1){
-	echo "You must be logged in to run this.  Goodbye.";
-	exit;	
+  echo "You must be logged in to run this.  Goodbye.";
+  exit;	
 }
 
 if (!defined("INDEX_CITRUS")) {
-	echo "You must be logged in to run this.  Goodbye.";
-        exit;
+  echo "You must be logged in to run this.  Goodbye.";
+  exit;
 }
 
+if (!isset($base->input['editremovaldate'])) { $base->input['editremovaldate'] = ""; }
+if (!isset($base->input['saveremovaldate'])) { $base->input['saveremovaldate'] = ""; }
+if (!isset($base->input['removaldate'])) { $base->input['removaldate'] = ""; }
+if (!isset($base->input['serviceid'])) { $base->input['serviceid'] = ""; }
 
+$editremovaldate = $base->input['editremovaldate'];
+$saveremovaldate = $base->input['saveremovaldate'];
+$removaldate = $base->input['removaldate'];
+$serviceid = $base->input['serviceid'];
+
+if ($editremovaldate) {
+  // prompt to change the removal date
+  echo "<FORM ACTION=\"index.php\" METHOD=\"GET\">".
+    "<input type=hidden name=load value=services>".
+    "<input type=hidden name=type value=module>".
+    "<input type=hidden name=history value=on>".
+    "<input type=hidden name=saveremovaldate value=\"on\">".     
+    "<input type=hidden name=serviceid value=\"$serviceid\">".
+    "<table>".
+    "<td>$l_new $l_removaldate:</td><td><input type=text name=removaldate ".
+    "value=\"$removaldate\"></td><tr>".
+    "<td></td><td><INPUT TYPE=\"SUBMIT\" NAME=\"submit\" ".
+    "value=\"$l_submitrequest\"></td>".
+    "</form>";
+ } else if ($saveremovaldate) {
+  // TODO: check that the new date entered is today or in the future, not the past
+  
+   $query = "UPDATE user_services SET removal_date = '$removaldate' ".
+     "WHERE id = '$serviceid'";
+   $result = $DB->Execute($query) or die ("due date update $l_queryfailed");
+
+    // redirect back to the service history for their account
+   echo "<script language=\"JavaScript\">window.location.href ".
+     "= \"index.php?load=services&type=module&history=on\";</script>";
+  
+ } else {
+  // print the history
 echo "
 <h3>$l_service $l_history</h3>
 <a href=\"index.php?load=services&type=module\">[ $l_back ]</a>
@@ -82,16 +118,17 @@ while ($myresult = $result->FetchRow())
 	// multiply the pricerate and the usage_multiple to get the price to show
 	$totalprice = $pricerate * $usage_multiple;
 
-	print "\n<tr onMouseOver='h(this);' onmouseout='deh(this);' onmousedown='window.location.href=\"index.php?load=services&type=module&edit=on&userserviceid=$id&servicedescription=$service_description&optionstable=$options_table&editbutton=Edit\";' bgcolor=\"#ddddee\">";
-	print "\n
-	<td>$id</td>
-	<td>$service_description</td>
-	<td>$optiondetails</td>
-	<td>$start_datetime</td>
-	<td>$end_datetime</td>
-	<td>$removal_date</td>
-	<td>$billing_id</td>
-	<td>$totalprice</td>
+	//print "\n<tr onMouseOver='h(this);' onmouseout='deh(this);' onmousedown='window.location.href=\"index.php?load=services&type=module&edit=on&userserviceid=$id&servicedescription=$service_description&optionstable=$options_table&editbutton=Edit\";' bgcolor=\"#ddddee\">";
+	
+	print "<tr bgcolor=\"#ddddee\">\n
+	<td onMouseOver='h(this);' onmouseout='deh(this);' onmousedown='window.location.href=\"index.php?load=services&type=module&edit=on&userserviceid=$id&servicedescription=$service_description&optionstable=$options_table&editbutton=Edit\";'>$id</td>
+	<td onMouseOver='h(this);' onmouseout='deh(this);' onmousedown='window.location.href=\"index.php?load=services&type=module&edit=on&userserviceid=$id&servicedescription=$service_description&optionstable=$options_table&editbutton=Edit\";'>$service_description</td>
+	<td onMouseOver='h(this);' onmouseout='deh(this);' onmousedown='window.location.href=\"index.php?load=services&type=module&edit=on&userserviceid=$id&servicedescription=$service_description&optionstable=$options_table&editbutton=Edit\";'>$optiondetails</td>
+	<td onMouseOver='h(this);' onmouseout='deh(this);' onmousedown='window.location.href=\"index.php?load=services&type=module&edit=on&userserviceid=$id&servicedescription=$service_description&optionstable=$options_table&editbutton=Edit\";'>$start_datetime</td>
+	<td onMouseOver='h(this);' onmouseout='deh(this);' onmousedown='window.location.href=\"index.php?load=services&type=module&edit=on&userserviceid=$id&servicedescription=$service_description&optionstable=$options_table&editbutton=Edit\";'>$end_datetime</td>
+	<td><a href=\"index.php?load=services&type=module&history=on&serviceid=$id&editremovaldate=on&removaldate=$removal_date\">$removal_date</a></td>
+	<td onMouseOver='h(this);' onmouseout='deh(this);' onmousedown='window.location.href=\"index.php?load=services&type=module&edit=on&userserviceid=$id&servicedescription=$service_description&optionstable=$options_table&editbutton=Edit\";'>$billing_id</td>
+	<td onMouseOver='h(this);' onmouseout='deh(this);' onmousedown='window.location.href=\"index.php?load=services&type=module&edit=on&userserviceid=$id&servicedescription=$service_description&optionstable=$options_table&editbutton=Edit\";'>$totalprice</td>
 	</td></tr>";
 } 
 print <<<END
@@ -99,6 +136,8 @@ print <<<END
 	</table>
 
 END;
+
+ } // end else all
 
 ?>
 
