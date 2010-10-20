19c10d7db33edd9133c9c3dba66c59f826eb8db6
Paul Yasi <paul@citrusdb.org>
2010-10-20 15:54:27 -0400

    add check in payment function to make sure there are rows returned to apply the payment to so it doesn't insert orphaned payment history items

--- tools/payment.php
+++ tools/payment.php
@@ -79,143 +79,152 @@ if ($submit) {
       "WHERE paid_amount < billed_amount AND billing_id = $billing_id";
     $DB->SetFetchMode(ADODB_FETCH_ASSOC);
     $result = $DB->Execute($query) or die ("select detail $l_queryfailed");	
-  }  
-  
-  /*-------------------------------------------------------------------*/
-  // Insert result into the Payment History
-  /*--------------------------------------------------------------------*/	
-  
-  // insert info into the payment history
-  $query = "INSERT INTO payment_history (creation_date, billing_id, ".
-    "billing_amount, status, payment_type, invoice_number, check_number) ".
-    "VALUES (CURRENT_DATE,'$billing_id','$payment',".
-    "'authorized','$payment_type','$invoice_number','$check_number')";
-  $paymentresult = $DB->Execute($query) or die ("$query insert history $l_queryfailed");
-
-  // get the payment history id that will be inserted into the billing_details
-  // items that are paid by this entry
-  $payment_history_id = $DB->Insert_ID();
-
-  /*------------------------------------------------------------------------*/
-  // go through the billing details items
-  /*------------------------------------------------------------------------*/  
-  while (($myresult = $result->FetchRow()) and (round($amount,2) > 0)) {
-    $id = $myresult['id'];
-    $paid_amount = sprintf("%.2f",$myresult['paid_amount']);
-    $billed_amount = sprintf("%.2f",$myresult['billed_amount']);
-    
-    // calculate amount owed
-    $owed = round($billed_amount - $paid_amount,2);
-    
-    // fix float precision too
-    
-    if (round($amount,2) >= round($owed,2)) {
-      $amount = round($amount - $owed, 2);
-      $fillamount = round($owed + $paid_amount,2);
-
-      $query = "UPDATE billing_details ".
-	"SET paid_amount = '$fillamount', ".
-	"payment_applied = CURRENT_DATE, ".
-	"payment_history_id = '$payment_history_id' ".	    
-	"WHERE id = $id";
-      $greaterthanresult = $DB->Execute($query)
-	or die ("detail update $l_queryfailed");
-    } else { 
-      // amount is  less than owed
-      $available = $amount;
-      $amount = 0;
-      $fillamount = round($available + $paid_amount,2);
-
-      $query = "UPDATE billing_details ".
-	"SET paid_amount = '$fillamount', ".
-	"payment_applied = CURRENT_DATE, ".
-	"payment_history_id = '$payment_history_id' ".	    	
-	"WHERE id = $id";
-      $lessthenresult = $DB->Execute($query) or die ("detail update $l_queryfailed");
-    } // end if amount >= owed
-  } // end while myresult and amount > 0
-
-
-  /*--------------------------------------------------------------------*/
-  // If the payment is made towards a prepaid account, then move
-  // the billing and payment dates forward for payment terms
-  /*--------------------------------------------------------------------*/
-  
-  //
-  // update the Next Billing Date to whatever the 
-  // billing type dictates +1 +2 +6 etc.
-  // get the current billing type
-  //
-
-  // Also select the customer's billing name, company, and address
-  // here to show up at the end to show what account paid.
-
-  $query = "SELECT b.billing_type b_billing_type, ".
-    "b.next_billing_date b_next_billing_date, ".
-    "b.from_date b_from_date, b.to_date b_to_date, ".
-    "b.name, b.company, b.street, b.city, b.state, ".
-    "b.account_number b_account_number, ".
-    "t.id t_id, t.frequency t_frequency, t.method t_method ".
-    "FROM billing b ".
-    "LEFT JOIN billing_types t ON b.billing_type = t.id ".
-    "WHERE b.id = $billing_id";
-  $DB->SetFetchMode(ADODB_FETCH_ASSOC);
-  $result = $DB->Execute($query) or die ("select next billing $l_queryfailed");
-  $billingresult = $result->fields;
-  $method = $billingresult['t_method'];
-  $billing_name = $billingresult['name'];
-  $billing_company = $billingresult['company'];
-  $billing_street = $billingresult['street'];
-  $billing_city = $billingresult['city'];
-  $billing_state = $billingresult['state'];
-  $billing_account_number = $billingresult['b_account_number'];
-  
-  // if they are prepay accounts the update their billing dates
-  if ($method == 'prepay' OR $method == 'prepaycc') {
-    $mybillingdate = $billingresult['b_next_billing_date'];
-    $myfromdate = $billingresult['b_from_date'];
-    $mytodate = $billingresult['b_to_date'];
-    $mybillingfreq = $billingresult['t_frequency'];
-    
-    // to get the to_date, need to double the frequency added
-    $doublefreq = $mybillingfreq * 2;	
-    
-    //
-    // insert the new next_billing_date
-    // and a new from_date and to_date 
-    // and payment_due_date based on from_date
-    //
-    
-    $query = "UPDATE billing SET ".
-      "next_billing_date = DATE_ADD('$mybillingdate', ".
-      "INTERVAL '$mybillingfreq' MONTH), ".
-      "from_date = DATE_ADD('$myfromdate', ".
-      "INTERVAL '$mybillingfreq' MONTH), ".
-      "to_date = DATE_ADD('$myfromdate', ".
-      "INTERVAL '$doublefreq' MONTH), ".
-      "payment_due_date = DATE_ADD('$myfromdate', ".
-      "INTERVAL '$mybillingfreq' MONTH) ".
-      "WHERE id = '$billing_id'"; 
-    $updateresult = $DB->Execute($query) or die ("update $l_queryfailed");
   }
 
-  if ($amount > 0) {
-    // if there is an over payment show the amount in red
-    //and prompt to add as a credit		
-    print "<h3 style=\"color: red;\">$l_paymentsaved, $l_currency$amount ".
-      "$l_leftover, <a href=\"index.php?load=addcredit&type=tools&amount=$amount&billing_id=$billing_id\">$l_addcreditfor $amount</a></h3>";
-  } else {
-    print "<h3>$l_paymentsaved: </h3>";
-  }
-
-  // print the customer billing information to confirm who the payment
-  // was entered for.
-
-  echo "<blockquote>$billing_account_number<br>$billing_name<br>".
-    "$billing_company<br>$billing_street<br>$billing_city $billing_state</blockquote><p>";
+  // make sure the result of the above queries returned some rows
+  $myrowcount = $result->RowCount();
+  if ($myrowcount > 0) {
   
+    /*-------------------------------------------------------------------*/
+    // Insert result into the Payment History
+    /*--------------------------------------------------------------------*/	
+    
+    // insert info into the payment history
+    $query = "INSERT INTO payment_history (creation_date, billing_id, ".
+      "billing_amount, status, payment_type, invoice_number, check_number) ".
+      "VALUES (CURRENT_DATE,'$billing_id','$payment',".
+      "'authorized','$payment_type','$invoice_number','$check_number')";
+    $paymentresult = $DB->Execute($query) or die ("$query insert history $l_queryfailed");
+    
+    // get the payment history id that will be inserted into the billing_details
+    // items that are paid by this entry
+    $payment_history_id = $DB->Insert_ID();
+    
+    /*------------------------------------------------------------------------*/
+    // go through the billing details items
+    /*------------------------------------------------------------------------*/  
+    while (($myresult = $result->FetchRow()) and (round($amount,2) > 0)) {
+      $id = $myresult['id'];
+      $paid_amount = sprintf("%.2f",$myresult['paid_amount']);
+      $billed_amount = sprintf("%.2f",$myresult['billed_amount']);
+      
+      // calculate amount owed
+      $owed = round($billed_amount - $paid_amount,2);
+      
+      // fix float precision too
+      
+      if (round($amount,2) >= round($owed,2)) {
+	$amount = round($amount - $owed, 2);
+	$fillamount = round($owed + $paid_amount,2);
+	
+	$query = "UPDATE billing_details ".
+	  "SET paid_amount = '$fillamount', ".
+	  "payment_applied = CURRENT_DATE, ".
+	  "payment_history_id = '$payment_history_id' ".	    
+	  "WHERE id = $id";
+	$greaterthanresult = $DB->Execute($query)
+	  or die ("detail update $l_queryfailed");
+      } else { 
+	// amount is  less than owed
+	$available = $amount;
+	$amount = 0;
+	$fillamount = round($available + $paid_amount,2);
+	
+	$query = "UPDATE billing_details ".
+	  "SET paid_amount = '$fillamount', ".
+	  "payment_applied = CURRENT_DATE, ".
+	  "payment_history_id = '$payment_history_id' ".	    	
+	  "WHERE id = $id";
+	$lessthenresult = $DB->Execute($query) or die ("detail update $l_queryfailed");
+      } // end if amount >= owed
+    } // end while myresult and amount > 0
+    
+    
+    /*--------------------------------------------------------------------*/
+    // If the payment is made towards a prepaid account, then move
+    // the billing and payment dates forward for payment terms
+    /*--------------------------------------------------------------------*/
+    
+    //
+    // update the Next Billing Date to whatever the 
+    // billing type dictates +1 +2 +6 etc.
+    // get the current billing type
+    //
+    
+    // Also select the customer's billing name, company, and address
+    // here to show up at the end to show what account paid.
+    
+    $query = "SELECT b.billing_type b_billing_type, ".
+      "b.next_billing_date b_next_billing_date, ".
+      "b.from_date b_from_date, b.to_date b_to_date, ".
+      "b.name, b.company, b.street, b.city, b.state, ".
+      "b.account_number b_account_number, ".
+      "t.id t_id, t.frequency t_frequency, t.method t_method ".
+      "FROM billing b ".
+      "LEFT JOIN billing_types t ON b.billing_type = t.id ".
+      "WHERE b.id = $billing_id";
+    $DB->SetFetchMode(ADODB_FETCH_ASSOC);
+    $result = $DB->Execute($query) or die ("select next billing $l_queryfailed");
+    $billingresult = $result->fields;
+    $method = $billingresult['t_method'];
+    $billing_name = $billingresult['name'];
+    $billing_company = $billingresult['company'];
+    $billing_street = $billingresult['street'];
+    $billing_city = $billingresult['city'];
+    $billing_state = $billingresult['state'];
+    $billing_account_number = $billingresult['b_account_number'];
+    
+    // if they are prepay accounts the update their billing dates
+    if ($method == 'prepay' OR $method == 'prepaycc') {
+      $mybillingdate = $billingresult['b_next_billing_date'];
+      $myfromdate = $billingresult['b_from_date'];
+      $mytodate = $billingresult['b_to_date'];
+      $mybillingfreq = $billingresult['t_frequency'];
+      
+      // to get the to_date, need to double the frequency added
+      $doublefreq = $mybillingfreq * 2;	
+      
+      //
+      // insert the new next_billing_date
+      // and a new from_date and to_date 
+      // and payment_due_date based on from_date
+      //
+      
+      $query = "UPDATE billing SET ".
+	"next_billing_date = DATE_ADD('$mybillingdate', ".
+	"INTERVAL '$mybillingfreq' MONTH), ".
+	"from_date = DATE_ADD('$myfromdate', ".
+	"INTERVAL '$mybillingfreq' MONTH), ".
+	"to_date = DATE_ADD('$myfromdate', ".
+	"INTERVAL '$doublefreq' MONTH), ".
+	"payment_due_date = DATE_ADD('$myfromdate', ".
+	"INTERVAL '$mybillingfreq' MONTH) ".
+	"WHERE id = '$billing_id'"; 
+      $updateresult = $DB->Execute($query) or die ("update $l_queryfailed");
+    }
+    
+    if ($amount > 0) {
+      // if there is an over payment show the amount in red
+      //and prompt to add as a credit		
+      print "<h3 style=\"color: red;\">$l_paymentsaved, $l_currency$amount ".
+	"$l_leftover, <a href=\"index.php?load=addcredit&type=tools&amount=$amount&billing_id=$billing_id\">$l_addcreditfor $amount</a></h3>";
+    } else {
+      print "<h3>$l_paymentsaved: </h3>";
+    }
+    
+    // print the customer billing information to confirm who the payment
+    // was entered for.
+    
+    echo "<blockquote>$billing_account_number<br>$billing_name<br>".
+      "$billing_company<br>$billing_street<br>$billing_city $billing_state</blockquote><p>";
+    
+  } // end if result RowCount > 0 
   
- } // end payment submit
+ else {
+   echo "no matching rows, $l_queryfailed";
+ }
+
+} // end payment submit
 
 
 //
