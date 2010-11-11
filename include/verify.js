8a588c2f33f1437134654256569b9470576fe599
Paul Yasi <paul@citrusdb.org>
2010-11-11 15:47:49 -0500

    change password length check from 6 to 7 to meet PCI section 8.5.10

--- include/verify.js
+++ include/verify.js
@@ -1,5 +1,5 @@
 function validatePassword(password) {
-	if (password.length < 6) { alert('Password must be at least 6 characters long'); return 0;
+	if (password.length < 7) { alert('Password must be at least 7 characters long'); return 0;
 	}
 	else { return 1; }
 }
