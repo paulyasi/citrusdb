75d3d0524e708bef621313b50b38a5a683ffd2ce
Paul Yasi <paul@citrusdb.org>
2011-01-05 15:28:35 -0500

    move url_prefix variables up with the other initial variables in the config file

--- include/config.inc.php
+++ include/config.inc.php
@@ -13,15 +13,13 @@ $sys_dbtype = 'mysql';
 $path_to_citrus = '/home/pyasi/citrus_project/citrusdb/';
 $hidden_hash_var='youmustchangethis';
 $lang = './include/local/us-english.inc.php';
+$url_prefix = "http://localhost/~pyasi/citrus_project/citrusdb/";
+$ssl_url_prefix = "https://localhost/~pyasi/citrus_project/citrusdb/";
 
 // these gpg commands are required for encrypted storage of credit card data
 $gpg_command = "/usr/bin/gpg --homedir /home/www-data/.gnupg --armor --batch -e -r 'CitrusDB'";
 $gpg_decrypt = "/usr/bin/gpg --homedir /home/www-data/.gnupg -v --passphrase-fd 0 --yes --no-tty --skip-verify --decrypt";
 $gpg_sign = "/usr/bin/gpg --homedir /home/www-data/.gnupg --passphrase-fd 0 --yes --no-tty --clearsign -u 'CitrusDB'";
-
-// use these url prefixes to redirect between the ssl carddata and regular pages
-$url_prefix = "http://localhost/~pyasi/citrus_project/citrusdb/";
-$ssl_url_prefix = "https://localhost/~pyasi/citrus_project/citrusdb/";
 
 // ldap server settings (optional for authentication)
 $ldap_enable = FALSE;
