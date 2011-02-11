c992075a9ca17127d321f1385a38d41d6bfe835d
Paul Yasi <paul@citrusdb.org>
2011-02-11 13:46:27 -0500

    fix query for email previous invoice link

--- tools/modules/billing/emailpreviousinvoice.php
+++ tools/modules/billing/emailpreviousinvoice.php
@@ -17,16 +17,16 @@ if (!defined("INDEX_CITRUS")) {
 // get the invoice data to process now
 $invoice_number = $base->input['invoiceid'];
 
-$query = "SELECT d.invoice_number, b.contact_email, b.id 
-FROM billing_details d
-LEFT JOIN billing b ON d.billing_id = b.id 
-WHERE d.invoice_number = '$invoice_number'";
+$query = "SELECT h.billing_id, b.contact_email ".
+  "FROM billing_history h ".
+  "LEFT JOIN billing b ON h.billing_id = b.id ".
+  "WHERE h.id = '$invoice_number'";
 $DB->SetFetchMode(ADODB_FETCH_ASSOC);
 $result = $DB->Execute($query) or die ("$l_queryfailed");
 $myresult = $result->fields;
 
 $contact_email = $myresult['contact_email'];
-$invoice_billing_id = $myresult['id'];
+$invoice_billing_id = $myresult['billing_id'];
 
 emailinvoice ($invoice_number,$contact_email,$invoice_billing_id);
 echo "sent invoice to $contact_email<br>\n";
