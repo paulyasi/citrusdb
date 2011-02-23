e08da85f8dfd4ec12b679c6f2fe715ef4f97002c
Paul Yasi <paul@citrusdb.org>
2011-02-23 16:29:08 -0500

    changed service growth to show numbers for each service gain/loss

--- tools/servicegrowth.php
+++ tools/servicegrowth.php
@@ -1,5 +1,5 @@
 <?php
-// Copyright (C) 2009 Paul Yasi (paul at citrusdb.org)
+// Copyright (C) 2011 Paul Yasi (paul at citrusdb.org)
 // Read the README file for more information
 
 /*----------------------------------------------------------------------------*/
@@ -20,6 +20,9 @@ if (!defined("INDEX_CITRUS")) {
 if (!isset($base->input['year'])) { $base->input['year'] = ""; }
 $year = $base->input['year'];
 
+if (!isset($base->input['month'])) { $base->input['month'] = ""; }
+$month = $base->input['month'];
+
 if (!isset($base->input['category'])) { $base->input['category'] = ""; }
 $category = $base->input['category'];
 
@@ -27,57 +30,62 @@ if ($year) {
   // print out a graph that compares each of the last 12 months
   // of service start_dates compared to end_dates
   $current  = date("Y-m-d", mktime(0, 0, 0, date("m")  , date("d"), date("Y")));
-  echo "$year $l_servicegrowth: $category";
-  echo "<table border=0>";
+  echo "$year service churn, month: $month<p>\n";
 
-  $month = array(1, 2, 3, 4, 5, 6, 7, 8, 9 , 10, 11, 12);
-
-  foreach($month as $mymonth) {
-    $query = "SELECT monthname(us.start_datetime) AS month, year(start_datetime), ".
+  echo "Gained:<br>\n";
+    $query = "SELECT ms.service_description, ms.id, monthname(us.start_datetime) AS month, year(start_datetime), ".
       "count(*) AS count FROM user_services us ".
       "LEFT JOIN master_services ms ON ms.id = us.master_service_id ".
-      "WHERE ms.category = '$category' AND YEAR(us.start_datetime) = '$year' ".
-      "AND MONTH(us.start_datetime) = '$mymonth' GROUP BY MONTH(us.start_datetime)";
+      "WHERE YEAR(us.start_datetime) = '$year' ".
+      "AND MONTH(us.start_datetime) = '$month' GROUP BY ms.id";
     $DB->SetFetchMode(ADODB_FETCH_ASSOC);
     $result = $DB->Execute($query) or die ("$query $l_queryfailed");
-    $myresult = $result->fields;
-    $count = $myresult['count'];
-    $monthvar = $monthname[$mymonth];
-    $countmultiplier = $count * 2;
-    $countpixels = "$countmultiplier" . "px";
-    echo "<td valign=bottom><div style=\"height: $countpixels; background-color: green;\"></div></td>\n";
-  }
-  
-  echo "<tr>";
+    while ($myresult = $result->FetchRow()) {
+      $count = $myresult['count'];
+      $service_description = $myresult['service_description'];
+      $mymonthname = $myresult['month'];
+      $msid = $myresult['id'];
+      echo "$service_description $count<br>\n";
+    }
 
-  // print months
-  echo "<td>$l_january</td><td>$l_february</td><td>$l_march</td><td>$l_april</td><td>$l_may</td><td>$l_june</td><td>$l_july</td><td>$l_august</td><td>$l_september</td><td>$l_october</td><td>$l_november</td><td>$l_december</td><tr>\n";
+  echo "<p>Lost<br>\n";  
 
-
-  foreach($month as $mymonth) {
-    $query = "SELECT monthname(us.end_datetime) AS month, year(end_datetime), ".
+    $query = "SELECT ms.service_description, ms.id, monthname(us.end_datetime) AS month, year(end_datetime), ".
       "count(*) AS count FROM user_services us ".
       "LEFT JOIN master_services ms ON ms.id = us.master_service_id ".
-      "WHERE ms.category = '$category' AND YEAR(us.end_datetime) = '$year' ".
-      "AND MONTH(us.end_datetime) = '$mymonth' GROUP BY MONTH(us.end_datetime)";
+      "WHERE YEAR(us.end_datetime) = '$year' ".
+      "AND MONTH(us.end_datetime) = '$month' GROUP BY ms.id";
     $DB->SetFetchMode(ADODB_FETCH_ASSOC);
     $result = $DB->Execute($query) or die ("$query $l_queryfailed");
-    $myresult = $result->fields;
-    $count = $myresult['count'];
-    $monthvar = $monthname[$mymonth];
-    $countmultiplier = $count * 2;
-    $countpixels = "$countmultiplier" . "px";
-    echo "<td valign=top><div style=\"height: $countpixels; background-color: red;\"></div></td>\n";
-  }
-  
-  echo "</table>";
-
+    while ($myresult = $result->FetchRow()) {
+      $count = $myresult['count'];
+      $service_description = $myresult['service_description'];
+      $mymonthname = $myresult['month'];
+      $msid = $myresult['id'];
+      echo "$service_description $count<br>\n";
+    }
+    
  } else {
   
   // print a form to ask what date you want to view
   echo "Enter year of to see service growth:";
 echo "<FORM ACTION=\"index.php\" METHOD=\"GET\">".
   "Year: <input type=text name=\"year\" value=\"$year\" size=4>".
+  "Month <select name=\"month\">".
+  "<option>1</option>".
+    "<option>2</option>".
+    "<option>3</option>".
+    "<option>4</option>".
+    "<option>5</option>".
+  "<option>6</option>".
+  "<option>7</option>".
+  "<option>8</option>".
+  "<option>9</option>".
+  "<option>10</option>".
+  "<option>11</option>".  
+  "<option>12<option></select>";
+
+/*
   "Category: <select name=\"category\">";
 // print list of categories to choose from
  $query = "SELECT DISTINCT category FROM master_services ".
@@ -88,7 +96,8 @@ echo "<FORM ACTION=\"index.php\" METHOD=\"GET\">".
    $categoryname = $myresult['category'];
    echo "<option value=\"$categoryname\">$categoryname</option> \n";
  }
- 
+*/
+
 echo  "</select><input type=hidden name=type value=tools>".
   "<input type=hidden name=load value=servicegrowth>".
   "&nbsp;<input type=submit name=\"$l_submit\" value=\"submit\">".
