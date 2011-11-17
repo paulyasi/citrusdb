85270682da3f9c6ded27e8ec3d4d8d1c062e3637
Paul Yasi <paul@citrusdb.org>
2011-11-17 15:55:51 -0500

    fixing URLs to check status for alternate billing types

--- application/views/billing/alternate_view.php
+++ application/views/billing/alternate_view.php
@@ -20,32 +20,32 @@ foreach ($alternate as $myresult)
 	// check if they are billing or manager and show the maintenance and rerun type links
 	if (($userprivileges['manager'] == 'y') OR ($userprivileges['admin'] == 'y')) {
 		echo "<td>".
-			"<a href=\"index.php/billing/rerund/$billing_id\">".
+			"<a href=\"$this->url_prefix/index.php/billing/rerun/$billing_id\">".
 			lang('rerun')."</a> | ".
-			"<a href=\"index.php/billing/invmaint/$billing_id&submit=Submit\">".
+			"<a href=\"$this->url_prefix/index.php/billing/invmaint/$billing_id\">".
 			lang('invoicemaintenance')."</a> | ".
-			"<a href=\"index.php/billing/refund/$billing_id&submit=Submit\">".
+			"<a href=\"$this->url_prefix/index.php/billing/refund/$billing_id\">".
 			lang('refund')."</a>".
 			"</td><td>".
 			"<form name=status$billing_id style=\"margin-bottom:0;\" method=post>".
 			"<select style=\"font-size: 80%;\" name=menu onChange=\"location=document.status".
 			"$billing_id.menu.options[document.status$billing_id.menu.selectedIndex].value;\" value=GO>".
 			"<option value=\"\">".lang('changestatus')."</option>".
-			"<option value=\"index.php/billing/turnoff/$billing_id\">- ".lang('turnoff')."</option>".
-			"<option value=\"index.php/billing/waiting/$billing_id\">- ".lang('waiting')."</optoin>".
-			"<option value=\"index.php/billing/authorized/$billing_id\">- ".lang('authorized')."</option>".
-			"<option value=\"index.php?load=billing/cancelwfee/$billing_id\">- ".lang('cancelwithfee')."</option>".
-			"<option value=\"index.php?load=billing/collections/$billing_id\">- ".lang('collections')."</option>".    
+			"<option value=\"$this->url_prefix/index.php/billing/turnoff/$billing_id\">- ".lang('turnoff')."</option>".
+			"<option value=\"$this->url_prefix/index.php/billing/waiting/$billing_id\">- ".lang('waiting')."</optoin>".
+			"<option value=\"$this->url_prefix/index.php/billing/authorized/$billing_id\">- ".lang('authorized')."</option>".
+			"<option value=\"$this->url_prefix/index.php/billing/cancelwfee/$billing_id\">- ".lang('cancelwithfee')."</option>".
+			"<option value=\"$this->url_prefix/index.php/billing/collections/$billing_id\">- ".lang('collections')."</option>".    
 			"</select></form>".
 			"</td><td>".
 			"<form name=notice$billing_id style=\"margin-bottom:0;\" method=post>".
 			"<select style=\"font-size: 80%;\" name=menu onChange=\"location=document.notice$billing_id.menu.".
 			"options[document.notice$billing_id.menu.selectedIndex].value;\" value=GO>".
 			"<option value=\"\">".lang('invoiceornotice')."</option>".
-			"<option value=\"index.php?load=billing/createinvoice/$billing_id\">- ".lang('createinvoice')."</option> | ".
-			"<option value=\"index.php?load=billing/cancelnotice/$billing_id\">- ".lang('cancel_notice')."</option> | ".
-			"<option value=\"index.php?load=billing/shutoffnotice/$billing_id\">- ".lang('shutoff_notice')."</option> | ".
-			"<option value=\"index.php?load=billing/collectionsnotice/$billing_id\">- ".lang('collections_notice')."</option>".
+			"<option value=\"$this->url_prefix/index.php/billing/createinvoice/$billing_id\">- ".lang('createinvoice')."</option> | ".
+			"<option value=\"$this->url_prefix/index.php/billing/cancelnotice/$billing_id\">- ".lang('cancel_notice')."</option> | ".
+			"<option value=\"$this->url_prefix/index.php/billing/shutoffnotice/$billing_id\">- ".lang('shutoff_notice')."</option> | ".
+			"<option value=\"$this->url_prefix/index.php/billing/collectionsnotice/$billing_id\">- ".lang('collections_notice')."</option>".
 			"</select></form>".
 			"</td>";
 	}
