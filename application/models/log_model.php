5a7cc23394268d6698b5769a894f937159027478
Paul Yasi <paul@citrusdb.org>
2011-12-15 13:54:44 -0500

    adding query binding to the log model

--- application/models/log_model.php
+++ application/models/log_model.php
@@ -10,22 +10,26 @@
 
 class Log_Model extends CI_Model
 {
+	
 	function __construct()
     {
         parent::__construct();
     }
-    
+
+	
 	function recently_viewed($user)
 	{
 		$query = "SELECT a.account_number, c.name FROM activity_log a 
 			LEFT JOIN customer c ON c.account_number = a.account_number 
-			WHERE a.user = '$user' AND activity_type = 'view' 
+			WHERE a.user = ? AND activity_type = 'view' 
 			AND record_type = 'customer' ORDER BY datetime DESC limit 10";
 		
-		$result = $this->db->query($query) or die ("$l_queryfailed");
+		$result = $this->db->query($query, array($user))
+			or die ("recently_viewed queryfailed");
 
 		return $result;
 	}
+	
     
 	function activity($user,$account_number,$activity_type,
 		$record_type,$record_id,$result)
@@ -41,18 +45,26 @@ class Log_Model extends CI_Model
 		if ($sys_dbtype == "mysql") 
 		{  
 			$query = "INSERT DELAYED INTO activity_log ".
-				"VALUES ('$datetime','$user','$ip_address',$account_number,".
-				"'$activity_type','$record_type','$record_id','$result')";
+				"VALUES (?,?,?,?,?,?,?,?)";
 		} 
 		else 
 		{
 			$query = "INSERT INTO activity_log ".
-			"VALUES ('$datetime','$user','$ip_address',$account_number,".
-			"'$activity_type','$record_type','$record_id','$result')";    
+			"VALUES (?,?,?,?,?,?,?,?)";    
 		}
 
-		$result = $this->db->query($query) or die ("activity_log insert failed");
+		$result = $this->db->query($query, array($datetime,
+												 $user,
+												 $ip_address,
+												 $account_number,
+												 $activity_type,
+												 $record_type,
+												 $record_id,
+												 $result))
+			or die ("activity_log insert failed");
   
 	} // end log_activity
 	
 }
+
+/* end log_model */
