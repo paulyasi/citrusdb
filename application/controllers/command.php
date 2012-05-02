dd0c999f6a29e0f2ad0db7cd274f56cb4669cae7
Paul Yasi <paul@citrusdb.org>
2012-05-02 10:47:35 -0400

    edit autoreceipt to use billing model

--- application/controllers/command.php
+++ application/controllers/command.php
@@ -345,7 +345,7 @@ class Command extends CI_Controller
 		$receiptresult = $this->billing_model->todays_receipts();
 
 		// go through each receipt recipient
-		while ($myreceiptresult = $receiptresult->FetchRow()) 
+		foreach ($receiptresult AS $myreceiptresult) 
 		{
 			// initialize the message body to be empty
 			$message = "";
@@ -358,15 +358,8 @@ class Command extends CI_Controller
 			$creditcard_number = $myreceiptresult['creditcard_number'];
 			$check_number = $myreceiptresult['check_number'];
 
-			/*-------------------------------------------------------------------------*/
 			// print paid_amounts from billing_details
-			/*-------------------------------------------------------------------------*/
-
-			$query = "SELECT * FROM billing b ".
-				"LEFT JOIN general g ON g.id = b.organization_id ".
-				"WHERE b.id = $billingid";
-			$DB->SetFetchMode(ADODB_FETCH_ASSOC);
-			$result = $DB->Execute($query) or die ("billing table Query Failed");
+			$result = $this->billing_model->billing_and_organization($billingid);
 			$myresult = $result->fields;
 			$billing_name = $myresult['name'];
 			$billing_company = $myresult['company'];
@@ -388,9 +381,8 @@ class Command extends CI_Controller
 
 			$human_date = humandate($payment_date, $lang);
 
-
-
-			if ($payment_type == "creditcard") {    
+			if ($payment_type == "creditcard") 
+			{    
 				// wipe out the middle of the card number
 				$length = strlen($creditcard_number);
 				$firstdigit = substr($creditcard_number, 0,1);
@@ -399,29 +391,19 @@ class Command extends CI_Controller
 
 				$message .= "$l_paid with $payment_type ($creditcard_number), ".
 					"$amount on $human_date for:\n";
-			} else {
+			} 
+			else 
+			{
 				$message .= "$l_paid with $payment_type (number: $check_number), ".
 					"$amount on $human_date for:\n";    
 			}
 
 			// get the resulting list of services that have payment applied with
 			// the matching payment_history_id
-			//$DB->debug = true;
+			$result = $this->billing_model->payment_details($paymentid);
 
-			$query = "SELECT bd.original_invoice_number, bd.paid_amount,".
-				"bh.from_date, bh.to_date, ".
-				"bd.billed_amount, ms.service_description, tr.description FROM ".
-				"billing_details bd ".
-				"LEFT JOIN user_services us ON us.id = bd.user_services_id ".
-				"LEFT JOIN master_services ms ON ms.id = us.master_service_id ".
-				"LEFT JOIN taxed_services ts ON ts.id = bd.taxed_services_id ".
-				"LEFT JOIN tax_rates tr ON tr.id = ts.tax_rate_id ".
-				"LEFT JOIN billing_history bh ON bh.id = bd.original_invoice_number ".
-				"WHERE bd.payment_history_id = '$paymentid' ORDER BY bd.taxed_services_id";
-			$DB->SetFetchMode(ADODB_FETCH_ASSOC);
-			$result = $DB->Execute($query) or die ("Receipt Query Failed");
-
-			while ($myresult = $result->FetchRow()) {
+			foreach($result AS $myresult) 
+			{
 				$invoice = $myresult['original_invoice_number'];
 				$description = $myresult['service_description'];
 				$tax_description = $myresult['description'];
@@ -432,10 +414,13 @@ class Command extends CI_Controller
 
 				$owed_amount = sprintf("%.2f",$billed_amount - $paid_amount);
 
-				if ($tax_description) {
+				if ($tax_description) 
+				{
 					// print the tax as description instead
 					$message .= "$invoice\t   $tax_description\t$paid_amount\n";
-				} else {
+				} 
+				else 
+				{
 					$message .= "$invoice\t$description ($from_date $l_to $to_date)\t$paid_amount\n";
 				}
 			}
