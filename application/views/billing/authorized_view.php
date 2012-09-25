2f6657a8ef899f027f36c61834d1208d92a4b1e8
Paul Yasi <paul@citrusdb.org>
2012-09-25 09:34:01 -0400

    fix No button in authorized view

--- application/views/billing/authorized_view.php
+++ application/views/billing/authorized_view.php
@@ -7,7 +7,7 @@
 <input type=hidden name=authorized value=on>
 <input type=hidden name=billing_id value=<?php echo $billing_id?>>
 <input name=save type=submit value=" <?php echo lang('yes');?> " class=smallbutton></form></td>
-<td align=left width=360><form style="margin-bottom:0;" "action="<?php echo $this->url_prefix?>/index.php/billing" method=post>
+<td align=left width=360><form style="margin-bottom:0;" action="<?php echo $this->url_prefix?>/index.php/billing" method=post>
 <input name=done type=submit value=" <?php echo lang('no');?>  " class=smallbutton>
 <input type=hidden name=load value=billing>
 <input type=hidden name=type value=module>
