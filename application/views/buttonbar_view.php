4bbd7865c621ec0c01b4b5ca15c01a86233ec900
Paul Yasi <paul@citrusdb.org>
2012-12-04 17:21:50 -0500

    add a dark navigation bar to the top

--- application/views/buttonbar_view.php
+++ application/views/buttonbar_view.php
@@ -1,23 +1,47 @@
-</div><div id="header" style="background-color: #BBB; width: 100%;">		
+</div><div id="header" style="background-color: #000; width: 100%;">		
 <table cellpadding=0 cellspacing=0 border=0 width=720>
 
 <td width="20%" align=center>
-<a href="<?php echo $this->url_prefix;?>/index.php/customer/newcustomer">
-<img src="<?php echo $this->url_prefix;?>/images/new-icon.png" alt="<?php echo lang('new')?>" border=0><br><?php echo lang('new')?></a></td>
+	<a href="<?php echo $this->url_prefix;?>/index.php/customer/newcustomer">
+		<table><td>
+		<img src="<?php echo $this->url_prefix;?>/images/new-icon.png" alt="<?php echo lang('new')?>" border=0></td>
+		<td style="color: #eee;"><?php echo lang('new')?>
+		</td></table>
+	</a>
+</td>
 <td width="20%" align=center>
-<a href="<?php echo $this->url_prefix;?>/index.php/dashboard">
-<img src="<?php echo $this->url_prefix;?>/images/search-icon.png" alt="<?php echo lang('search')?>" border=0><br><?php echo lang('search')?></a></td>
+	<a href="<?php echo $this->url_prefix;?>/index.php/dashboard">
+		<table><td>
+		<img src="<?php echo $this->url_prefix;?>/images/search-icon.png" alt="<?php echo lang('search')?>" border=0></td>
+		<td style="color: #eee;"><?php echo lang('search')?>
+		</td></table>
+	</a>
+</td>
 <td width="20%" align=center>
-<a href="<?php echo $this->url_prefix;?>/index.php/reports">
-<img src="<?php echo $this->url_prefix;?>/images/reports-icon.png" alt="<?php echo lang('reports')?>" border=0><br><?php echo lang('reports')?></a></td>
+	<a href="<?php echo $this->url_prefix;?>/index.php/reports">
+		<table><td>
+		<img src="<?php echo $this->url_prefix;?>/images/reports-icon.png" alt="<?php echo lang('reports')?>" border=0></td>
+		<td style="color: #eee;"><?php echo lang('reports')?>
+		</td></table>
+	</a>
+</td>
 <td width="20%" align=center>
-<a href="<?php echo $this->url_prefix;?>/index.php/tools">
-<img src="<?php echo $this->url_prefix;?>/images/admin-icon.png" alt="<?php echo lang('tools');?>" border=0><br><?php echo lang('tools');?></a></td>
+	<a href="<?php echo $this->url_prefix;?>/index.php/tools">
+		<table><td>
+		<img src="<?php echo $this->url_prefix;?>/images/admin-icon.png" alt="<?php echo lang('tools');?>" border=0></td>
+		<td style="color: #eee;"><?php echo lang('tools');?>
+		</td></table>
+	</a>
+</td>
 <td width="20%" align=center>
-
-<a href="<?php echo $this->url_prefix;?>/index.php/session/logout">
-<img src="<?php echo $this->url_prefix;?>/images/logout-icon.png" alt="<?php echo lang('logout'); echo " $this->user"; ?>" border=0>
-<br><?php echo lang('logout'); echo " $this->user"; ?></a><td><tr>
-</td></table>
+	<a href="<?php echo $this->url_prefix;?>/index.php/session/logout">
+		<table><td>
+		<img src="<?php echo $this->url_prefix;?>/images/logout-icon.png" alt="<?php echo lang('logout'); echo " $this->user"; ?>" border=0></td>
+		<td style="color: #eee;"><?php echo lang('logout'); echo " $this->user"; ?>
+		</td></table>
+	</a>
+<td><tr>
+</td>
+</table>
 </div><div id="content">
 
