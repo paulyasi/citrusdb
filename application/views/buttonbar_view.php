529f72c337877a13f4bedd1f7fa69bd847bdd90e
Paul Yasi <paul@citrusdb.org>
2012-12-10 13:46:54 -0500

    switch back to sidebar style, but with div between logo to make it more flexible

--- application/views/buttonbar_view.php
+++ application/views/buttonbar_view.php
@@ -1,13 +1,12 @@
-<div id="header" style="background-color: #000; width: 100%;">
-	<div class="buttonbaritem" style="padding-top: 3px; color: white; font-family: DejaVu Sans, Droid Sans, sans-serif; font-size: large;">
-		<img height=20 src="<?php echo $this->url_prefix;?>/images/citrus_wedge.png">citrusdb
+<div id="header" style="width: 100%;">
+	<div class="buttonbaritem" style="color: white; font-family: DejaVu Sans, Droid Sans, sans-serif; font-size: x-large; background-color: #eee; width: 200px; height: 100%;">
+		<img src="<?php echo $this->url_prefix;?>/images/my-logo.png">
 	</div>
 	<div class="buttonbaritem">
 		<a href="<?php echo $this->url_prefix;?>/index.php/customer/newcustomer">
 		<div style="float: left;">
 			<img src="<?php echo $this->url_prefix;?>/images/new-icon.png" alt="<?php echo lang('new')?>" border=0>
-		</div>
-		<div style="float: left;">
+			<br>
 			<?php echo lang('new')?>
 		</div>
 		</a>
@@ -16,8 +15,7 @@
 		<a href="<?php echo $this->url_prefix;?>/index.php/dashboard">
 		<div style="float: left;">
 		<img src="<?php echo $this->url_prefix;?>/images/search-icon.png" alt="<?php echo lang('search')?>" border=0>
-		</div>
-		<div style="float: left;">
+			<br>
 		<?php echo lang('search')?>
 		</div>
 		</a>
@@ -26,8 +24,7 @@
 		<a href="<?php echo $this->url_prefix;?>/index.php/reports">
 		<div style="float: left;">
 			<img src="<?php echo $this->url_prefix;?>/images/reports-icon.png" alt="<?php echo lang('reports')?>" border=0>
-		</div>
-		<div style="float: left;">
+			<br>
 			<?php echo lang('reports')?>
 		</div>
 		</a>
@@ -36,8 +33,7 @@
 		<a href="<?php echo $this->url_prefix;?>/index.php/tools">
 		<div style="float: left;">
 		<img src="<?php echo $this->url_prefix;?>/images/admin-icon.png" alt="<?php echo lang('tools');?>" border=0>
-		</div>
-		<div style="float: left;">
+			<br>
 		<?php echo lang('tools');?>
 		</div>
 		</a>
@@ -46,8 +42,7 @@
 		<a href="<?php echo $this->url_prefix;?>/index.php/session/logout">
 		<div style="float: left;">
 		<img src="<?php echo $this->url_prefix;?>/images/logout-icon.png" alt="<?php echo lang('logout'); echo " $this->user"; ?>" border=0>
-		</div>
-		<div style="float: left;">
+			<br>
 		<?php echo lang('logout'); echo " $this->user"; ?>
 		</div>
 	</a>
--- application/views/customer_in_sidebar_view.php
+++ application/views/customer_in_sidebar_view.php
@@ -1,5 +1,3 @@
-<p align=center>
-<img src="<?php echo $this->url_prefix;?>/images/my-logo.png"></p>
 <p align=center class="smalltext">
 <b><?php echo lang('accountnum')?>:&nbsp; <?php echo $this->account_number?> &nbsp; </b><br>
 <?php echo $acct_name;?><br>
