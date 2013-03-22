f870169a8dce30f7371765e27b059cb42c963812
Paul Yasi <paul@citrusdb.org>
2013-03-22 15:00:51 -0400

    remove extra table definitions from service search view

--- application/views/services/search_view.php
+++ application/views/services/search_view.php
@@ -5,7 +5,7 @@
 <td valign=top>
 <form ACTION="<?php echo $this->url_prefix;?>/index.php/search/results/1/20" 
 METHOD="POST">
-<?php echo lang('exampleusername') ?></td><td><input type=text name=s1 size=32>
+<?php echo lang('exampleusername') ?>&nbsp;<input type=text name=s1>
 <input type=hidden name=id value=9> <!-- the id of this search in the searches table -->
 <input type=submit name=submit value=<?php echo lang('search');?> class=smallbutton>
 </form>
@@ -13,7 +13,7 @@ METHOD="POST">
 <td valign=top>
 <form ACTION="<?php echo $this->url_prefix;?>/index.php/search/results/1/20" 
 METHOD="POST">
-<?php echo lang('examplepassword'); ?> </td><td><input type=text name=s1 size=32>
+<?php echo lang('examplepassword'); ?>&nbsp;<input type=text name=s1>
 <input type=hidden name=id value=10> <!-- the id of this search in the searches table -->
 <input type=submit name=submit value=<?php echo lang('search');?> class=smallbutton>
 </form>
@@ -21,7 +21,7 @@ METHOD="POST">
 <td valign=top>
 <form ACTION="<?php echo $this->url_prefix;?>/index.php/search/results/1/20" 
 METHOD="POST">
-<?php echo lang('exampleequipment'); ?> </td><td><input type=text name=s1 size=32>
+<?php echo lang('exampleequipment'); ?>&nbsp;<input type=text name=s1>
 <input type=hidden name=id value=11> <!-- the id of this search in the searches table -->
 <input type=submit name=submit value=<?php echo lang('search');?> class=smallbutton>
 </form>
