85c5e7c0e1cd4265006f9c1cefaf7d399a04f06b
Paul Yasi <paul@citrusdb.org>
2013-03-22 12:33:12 -0400

    make recently viewed account list always the same height

--- application/views/recently_viewed_view.php
+++ application/views/recently_viewed_view.php
@@ -1,12 +1,12 @@
 <p align=center><b>Recently Viewed:</b>
-<table cellpadding=10><td class="smalltext">
+<!--<table cellpadding=10><td class="smalltext"> -->
 
 <?php foreach($recent->result() as $customer):?>
 
-  <a href="
+  <div style="text-align: center; height: 14px; white-space:nowrap;"><a href="
   <?php echo $this->url_prefix?>/index.php/view/account/<?php echo $customer->account_number;?>
   ">
-  <?php echo $customer->account_number . ": " . $customer->name?></a><br>
+  <?php echo $customer->account_number . ": " . $customer->name?></a></div>
 
 <?php endforeach;?>
-</td></table></p>
+<!--</td></table>--></p>
