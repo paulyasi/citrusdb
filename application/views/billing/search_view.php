9848ed8a28a22812cf79cbc2cfd3153ce1e19f4d
Paul Yasi <paul@citrusdb.org>
2013-03-22 15:06:02 -0400

    removed extra table formatting from billing and support search view

--- application/views/billing/search_view.php
+++ application/views/billing/search_view.php
@@ -5,7 +5,7 @@
 <td valign=top>
 <form ACTION="<?php echo $this->url_prefix;?>/index.php/search/results/1/20" 
 METHOD="POST">
-<?php echo lang('billingid'); ?> </td><td><input type=text name=s1>
+<?php echo lang('billingid'); ?> &nbsp;<input type=text name=s1>
 <input type=hidden name=id value=5> <!-- the id of this search in the searches table -->
 <input type=submit name=submit value="<?php echo lang('search');?>" class=smallbutton>
 </form>
@@ -13,14 +13,14 @@ METHOD="POST">
 </td><tr><td valign=top>
 <form ACTION="<?php echo $this->url_prefix;?>/index.php/search/results/1/20" 
 METHOD="POST">
-<?php echo lang('billingcompany'); ?></td><td><input type=text name=s1>
+<?php echo lang('billingcompany'); ?>&nbsp;<input type=text name=s1>
 <input type=hidden name=id value=6> <!-- the id of this search in the searches table -->
 <input type=submit name=submit value=<?php echo lang('search');?> class=smallbutton>
 </form>
 </td><tr><td valign=top>
 <form ACTION="<?php echo $this->url_prefix;?>/index.php/search/results/1/20" 
 METHOD="POST">
-<?php echo lang('paymentduedate');?></td><td><input type=text name=s1>
+<?php echo lang('paymentduedate');?>&nbsp;<input type=text name=s1>
 <input type=hidden name=id value=7> <!-- the id of this search in the searches table -->
 <input type=submit name=submit value=<?php echo lang('search');?> class=smallbutton>
 </form>
@@ -28,7 +28,7 @@ METHOD="POST">
 </td><tr><td valign=top>
 <form ACTION="<?php echo $this->url_prefix;?>/index.php/search/results/1/20" 
 METHOD="POST">
-<?php echo lang('invoicenumber'); ?></td><td><input type=text name=s1>
+<?php echo lang('invoicenumber'); ?>&nbsp;<input type=text name=s1>
 <input type=hidden name=id value=13> <!-- the id of this search in the searches table -->
 <input type=submit name=submit value=<?php echo lang('search');?> class=smallbutton>
 </form>
@@ -36,7 +36,7 @@ METHOD="POST">
 </td><tr><td valign=top>
 <form ACTION="<?php echo $this->url_prefix;?>/index.php/search/results/1/20" 
 METHOD="POST">
-<?php echo lang('contactemail'); ?></td><td><input type=text name=s1>
+<?php echo lang('contactemail'); ?>&nbsp;<input type=text name=s1>
 <input type=hidden name=id value=14> <!-- the id of this search in the searches table -->
 <input type=submit name=submit value=<?php echo lang('search');?> class=smallbutton>
 </form>
@@ -44,8 +44,7 @@ METHOD="POST">
 </td><tr><td valign=top>
 <form ACTION="<?php echo $this->url_prefix;?>/index.php/search/results/1/20" 
 METHOD="POST">
-<?php echo lang('nextbillingdate'); ?></td>
-<td><input type=text name=s1 size=9><?php echo lang('to'); ?><input type=text name=s2 size=9>
+<?php echo lang('nextbillingdate'); ?>&nbsp;<input type=text name=s1 size=5><?php echo lang('to'); ?><input type=text name=s2 size=5>
    <input type=hidden name=id value=15>
    <!-- the id of this search in the searches table -->
 <input type=submit name=submit value=<?php echo lang('search');?> class=smallbutton>
--- application/views/support/search_view.php
+++ application/views/support/search_view.php
@@ -5,7 +5,7 @@
 <td valign=top>
 <form ACTION="<?php echo $this->url_prefix;?>index.php/search/results/1/20" 
 METHOD="POST">
-<?php echo lang('ticketnumber'); ?> </td><td><input type=text name=s1 size=32>
+<?php echo lang('ticketnumber'); ?> &nbsp;<input type=text name=s1>
 <input type=hidden name=id value=8> <!-- the id of this search in the searches table -->
 <input type=submit name=submit value="<?php echo lang ('search');?>" class=smallbutton>
 </form>
@@ -13,7 +13,7 @@ METHOD="POST">
 </td><tr><td valign=top>
 <form ACTION="<?php echo $this->url_prefix;?>index.php/search/results/1/20" 
 METHOD="POST">
-<?php echo lang('creationdate'); ?> </td><td><input type=text name=s1 size=9>
+<?php echo lang('creationdate'); ?> &nbsp;<input type=text name=s1 size=5>
 <input type=hidden name=id value=16> <!-- the id of this search in the searches table -->
 <input type=submit name=submit value="<?php echo lang('search')?>" class=smallbutton>
 </form>
@@ -21,9 +21,9 @@ METHOD="POST">
 </td><tr><td valign=top>
 <form ACTION="<?php echo $this->url_prefix;?>index.php/search/results/1/20" 
 METHOD="POST">
-<?php echo lang('createdby'); ?> </td><td><input type=text name=s1 size=9>
+<?php echo lang('createdby'); ?> &nbsp;<input type=text name=s1 size=5>
 <input type=hidden name=id value=17> <!-- the id of this search in the searches table -->
-<input type=submit name=submit value="<?php lang('search');?>" class=smallbutton>
+<input type=submit name=submit value="<?php echo lang('search');?>" class=smallbutton>
 </form>
 
 </td></table>
