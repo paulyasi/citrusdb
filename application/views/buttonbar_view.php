67f1247f734202e7c6b965acd85b624c37b321ed
Paul Yasi <paul@citrusdb.org>
2013-03-22 10:53:42 -0400

    responsive buttons in header

--- application/views/buttonbar_view.php
+++ application/views/buttonbar_view.php
@@ -4,47 +4,37 @@
 	</div>
 	<div class="buttonbaritem">
 		<a href="<?php echo $this->url_prefix;?>/index.php/customer/newcustomer">
-		<div style="float: left;">
 			<img src="<?php echo $this->url_prefix;?>/images/new-icon.png" alt="<?php echo lang('new')?>" border=0>
 			<br>
 			<?php echo lang('new')?>
-		</div>
 		</a>
 	</div>
 	<div class="buttonbaritem">
 		<a href="<?php echo $this->url_prefix;?>/index.php/dashboard">
-		<div style="float: left;">
 		<img src="<?php echo $this->url_prefix;?>/images/search-icon.png" alt="<?php echo lang('search')?>" border=0>
 			<br>
 		<?php echo lang('search')?>
-		</div>
 		</a>
 	</div>
 	<div class="buttonbaritem">
 		<a href="<?php echo $this->url_prefix;?>/index.php/reports">
-		<div style="float: left;">
 			<img src="<?php echo $this->url_prefix;?>/images/reports-icon.png" alt="<?php echo lang('reports')?>" border=0>
 			<br>
 			<?php echo lang('reports')?>
-		</div>
 		</a>
 	</div>
 	<div class="buttonbaritem">
 		<a href="<?php echo $this->url_prefix;?>/index.php/tools">
-		<div style="float: left;">
 		<img src="<?php echo $this->url_prefix;?>/images/admin-icon.png" alt="<?php echo lang('tools');?>" border=0>
 			<br>
 		<?php echo lang('tools');?>
-		</div>
 		</a>
 	</div>
 	<div class="buttonbaritem">
 		<a href="<?php echo $this->url_prefix;?>/index.php/session/logout">
-		<div style="float: left;">
 		<img src="<?php echo $this->url_prefix;?>/images/logout-icon.png" alt="<?php echo lang('logout'); echo " $this->user"; ?>" border=0>
 			<br>
 		<?php echo lang('logout'); echo " $this->user"; ?>
-		</div>
 	</a>
 	</div>
 </div>
