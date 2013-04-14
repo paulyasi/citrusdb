86825132993f938a0bd3eda19f5cf8bb08fd4740
Paul Yasi <paul@citrusdb.org>
2013-04-14 11:27:30 -0400

    change icons to show to the right of the text in the top bar

--- application/views/buttonbar_view.php
+++ application/views/buttonbar_view.php
@@ -1,42 +1,37 @@
 <div id="header">
-	<div class="buttonbaritem">
-		<img class="full-image" src="<?php echo $this->url_prefix;?>/images/my-logo.png">
-	</div>
-	<div class="buttonbaritem">
-		<a href="<?php echo $this->url_prefix;?>/index.php/customer/newcustomer">
-			<img src="<?php echo $this->url_prefix;?>/images/new-icon.png" alt="<?php echo lang('new')?>" border=0>
-			<br>
-			<?php echo lang('new')?>
-		</a>
-	</div>
-	<div class="buttonbaritem">
-		<a href="<?php echo $this->url_prefix;?>/index.php/dashboard">
-		<img src="<?php echo $this->url_prefix;?>/images/search-icon.png" alt="<?php echo lang('search')?>" border=0>
-			<br>
-		<?php echo lang('search')?>
-		</a>
-	</div>
-	<div class="buttonbaritem">
-		<a href="<?php echo $this->url_prefix;?>/index.php/reports">
-			<img src="<?php echo $this->url_prefix;?>/images/reports-icon.png" alt="<?php echo lang('reports')?>" border=0>
-			<br>
-			<?php echo lang('reports')?>
-		</a>
-	</div>
-	<div class="buttonbaritem">
-		<a href="<?php echo $this->url_prefix;?>/index.php/tools">
-		<img src="<?php echo $this->url_prefix;?>/images/admin-icon.png" alt="<?php echo lang('tools');?>" border=0>
-			<br>
-		<?php echo lang('tools');?>
-		</a>
-	</div>
-	<div class="buttonbaritem">
-		<a href="<?php echo $this->url_prefix;?>/index.php/session/logout">
-		<img src="<?php echo $this->url_prefix;?>/images/logout-icon.png" alt="<?php echo lang('logout'); echo " $this->user"; ?>" border=0>
-			<br>
-		<?php echo lang('logout'); echo " $this->user"; ?>
-	</a>
-	</div>
+  <div class="buttonbaritem">
+    <img class="full-image"
+	 src="<?php echo $this->url_prefix;?>/images/my-logo.png">
+  </div>
+  <div class="buttonbaritem">
+    <a href="<?php echo $this->url_prefix;?>/index.php/customer/newcustomer"
+      style="background: transparent url(<?php echo $this->url_prefix;?>/images/new-icon.png) scroll no-repeat left center; padding: 2px 0px 2px 25px;">
+      <?php echo lang('new')?>
+    </a>
+  </div>
+  <div class="buttonbaritem">
+    <a href="<?php echo $this->url_prefix;?>/index.php/dashboard"
+      style="background: transparent url(<?php echo $this->url_prefix;?>/images/search-icon.png) scroll no-repeat left center; padding: 2px 0px 2px 25px;">
+      <?php echo lang('search')?>
+    </a>
+  </div>
+  <div class="buttonbaritem">
+    <a href="<?php echo $this->url_prefix;?>/index.php/reports"
+      style="background: transparent url(<?php echo $this->url_prefix;?>/images/reports-icon.png) scroll no-repeat left center; padding: 2px 0px 2px 25px;">
+      <?php echo lang('reports')?>
+    </a>
+  </div>
+  <div class="buttonbaritem">
+    <a href="<?php echo $this->url_prefix;?>/index.php/tools"
+       style="background: transparent url(<?php echo $this->url_prefix;?>/images/admin-icon.png) scroll no-repeat left center; padding: 2px 0px 2px 25px;">
+      <?php echo lang('tools');?>
+    </a>
+  </div>
+  <div class="buttonbaritem">
+    <a href="<?php echo $this->url_prefix;?>/index.php/session/logout"
+       style="background: transparent url(<?php echo $this->url_prefix;?>/images/logout-icon.png) scroll no-repeat left center; padding: 2px 0px 2px 25px;"><?php echo lang('logout'); echo " $this->user"; ?>
+    </a>
+  </div>
 </div>
 <div id="sidebar">
 
