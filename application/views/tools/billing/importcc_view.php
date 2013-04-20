67f072ce4e34ded79f8767ccd3b2c5f46eabfb01
Paul Yasi <paul@citrusdb.org>
2013-04-20 13:23:25 -0400

    making some things fit betting in the tools popup window

--- application/views/tools/billing/importcc_view.php
+++ application/views/tools/billing/importcc_view.php
@@ -5,7 +5,7 @@
 <FORM ACTION="<?php echo $this->ssl_url_prefix?>/index.php/tools/billing/saveimportcc" METHOD="POST" enctype="multipart/form-data">
 <table>
 <td><?php echo lang('importfile')?>:</td>
-<td><input type=file name="userfile" size=50></td><tr> 
+<td><input type=file name="userfile" size=20></td><tr> 
 <td></td><td><br>
 <input type=submit name="<?php echo lang('import')?>" value="<?php echo lang('import')?>">
 </td>
--- application/views/tools/user/version_view.php
+++ application/views/tools/user/version_view.php
@@ -1,14 +1,12 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 ?>
 <center>
-<table height=90% width=100%>
-<td align=center>
-<b><img alt="citrusdb" src="../../images/citrus-logo.png"><p><?php echo lang('version')?> 
-<?php echo $this->softwareversion?></b><p>
+<b><img alt="citrusdb" src="../../../images/citrus-logo.png"><p><?php echo lang('version')?> 
+<?php echo $this->softwareversion?></b>
+<p>
 <a href="http://www.citrusdb.org" target="_blank">http://www.citrusdb.org</a>
 <p>
 PHP: <?php print phpversion(); ?>
-
 <br>
 
 <?php
@@ -16,13 +14,11 @@ echo lang('database').": ";
 echo $this->db->dbdriver." ".$this->db->version();
 ?>
 
-<br><br><br>
 <hr width=450 noshade size=1>
-<br><br><br>
-<table><td width=400>
+<br><br>
+<table width=400><td>
 <pre><? include ('./README'); ?></pre>
-</td>
-</table>
+</td></table>
 </center>
 
 
